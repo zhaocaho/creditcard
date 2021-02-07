85546fda58042f353968bcb4c462d41ff37c5627
zhaochao <243211921@qq.com>
2021-02-07 10:51:09 +0800

    test

---
+++ src/views/Home/component/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Register">
+    注册
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Register'
+})
+</script>
+
+<style lang="scss" scoped>
+.Register {
+  font-size: 0.5rem;
+}
+</style>
